cf0e94599c6ddf394a2b549a786771a76531d3d7
Unknown <gud@iflow.ch>
2018-05-01 21:05:12 +0200

    Started and Aborted Aufg 4

---
+++ oop_maven_template/src/main/java/ch/hslu/ad/sw10/Aufg4/FixedSizeHeap.java
@@ -0,0 +1,45 @@
+package ch.hslu.ad.sw10.Aufg4;
+
+public class FixedSizeHeap {
+	private int[] array;
+
+	private final class Node {
+		private Node parent;
+		private Node leftChild;
+		private Node rightChild;
+
+		public Node(final Node parent, final Node leftChild, final Node rightChild) {
+			this.parent = parent;
+			this.leftChild = leftChild;
+			this.rightChild = rightChild;
+		}
+
+		private Node getLeftChild() {
+			return this.leftChild;
+		}
+
+		private Node getRightChild() {
+			return this.rightChild;
+		}
+
+		private Node getParent() {
+			return this.parent;
+		}
+
+		private void setLeftChild(final Node leftChild) {
+			this.leftChild = leftChild;
+		}
+
+		private void setRightChild(final Node rightChild) {
+			this.rightChild = rightChild;
+		}
+
+		private void setParent(final Node parent) {
+			this.parent = parent;
+		}
+	}
+
+	public FixedSizeHeap(final int[] array) {
+		for (int i = )
+	}
+}
---
+++ oop_maven_template/src/main/java/ch/hslu/ad/sw10/Aufg4/IntegerHeap.java
@@ -0,0 +1,9 @@
+package ch.hslu.ad.sw10.Aufg4;
+
+public interface IntegerHeap {
+
+	public void insert();
+
+	public void remove();
+
+}
