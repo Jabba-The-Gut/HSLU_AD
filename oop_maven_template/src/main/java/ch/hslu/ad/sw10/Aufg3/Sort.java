ce5595f9cbcf626a60e1daa98d7d454d95e0f6a7
Unknown <gud@iflow.ch>
2018-05-07 22:05:16 +0200

    Corrected Error

--- oop_maven_template/src/main/java/ch/hslu/ad/sw10/Aufg3/Sort.java
+++ oop_maven_template/src/main/java/ch/hslu/ad/sw10/Aufg3/Sort.java
@@ -244,19 +244,51 @@ public class Sort {
 			}
 		} while (!allChecked);
 		exchange(a, up, right); // Trennelement an endgültige Position (a[up])
-
-		// Hier ist noch ein Fehler drin. Ich sollte in der Else-condition wieder
-		// QuickInsertionSort aufrufen!!
-		if ((left - (up - 1)) == TRESHOLD) {
+		if ((left - (up - 1)) < TRESHOLD) {
 			Sort.insertionSort(a);
 		} else {
-			quickSort(a, left, (up - 1)); // linke Hälfte
+			quickInsertionSort(a, left, (up - 1), TRESHOLD); // linke Hälfte
 		}
-		if (((up + 1) - right) == TRESHOLD) {
+		if (((up + 1) - right) < TRESHOLD) {
 			Sort.insertionSort(a);
 		} else {
 			if ((up + 1) < right)
-				quickSort(a, (up + 1), right); // rechte Hälfte, ohne T’Elt.}
+				quickInsertionSort(a, (up + 1), right, TRESHOLD); // rechte Hälfte, ohne T’Elt.}
+		}
+	}
+	
+	public static final void quickInsertionSort(final char[] a,final int left, final int right, final int TRESHOLD) {
+		
+		int up = left; // linke Grenze
+		int down = right - 1; // rechte Grenze (ohne Trennelement)
+		char t = a[right]; // rechtes Element als Trennelement
+		boolean allChecked = false;
+		do {
+			while (a[up] < t) {
+				up++; // suche grösseres (>=) Element von links an
+			}
+			while ((a[down] >= t) && (down > up)) {
+				down--; // suche echt kleineres(<) Element von rechts an
+			}
+			if (down > up) { // solange keine Überschneidung
+				exchange(a, up, down);
+				up++;
+				down--; // linke und rechte Grenze verschieben
+			} else {
+				allChecked = true; // Austauschen beendet
+			}
+		} while (!allChecked);
+		exchange(a, up, right); // Trennelement an endgültige Position (a[up])
+		if ((left - (up - 1)) < TRESHOLD) {
+			Sort.insertionSort(a);
+		} else {
+			quickInsertionSort(a, left, (up - 1), TRESHOLD); // linke Hälfte
+		}
+		if (((up + 1) - right) < TRESHOLD) {
+			Sort.insertionSort(a);
+		} else {
+			if ((up + 1) < right)
+				quickInsertionSort(a, (up + 1), right, TRESHOLD); // rechte Hälfte, ohne T’Elt.}
 		}
 	}
 }
